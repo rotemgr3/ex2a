02c214d2f34ae52da807f9010d175d93042094a4
rotemgr3 <rotemgr3@gmail.com>
2021-06-04 12:08:24 +0300

    added examDetails implementation

---
+++ examDetails.cpp
@@ -0,0 +1,93 @@
+#include <ostream>
+#include <string>
+#include "examDetails.h"
+
+using namespace mtm;
+
+const int mtmExam_course_number = 234124;
+const int mtmExam_month = 7;
+const int mtmExam_day = 28;
+const double mtmExam_time = 13;
+const int mtmExam_duration = 3;
+const int days_in_month = 30;
+const std::string mtmExam_link = "https://tinyurl.com/59hzps6m";
+
+ExamDetails::ExamDetails(int course_number, int month, int day, double time, int duration, std::string link= "") : 
+    course_number(course_number),
+    month(month),
+    day(day),
+    time(time),
+    duration(duration),
+    link(link)
+{
+}
+ExamDetails::ExamDetails(const ExamDetails& exam) : 
+    course_number(exam.course_number) ,
+    month(exam.month) , 
+    day(exam.day) ,
+    time(exam.time) ,
+    duration(exam.duration) ,
+    link(exam.link)
+{
+}
+ExamDetails ExamDetails::makeMatamExam()
+{
+    return ExamDetails(mtmExam_course_number,
+                    mtmExam_month,
+                    mtmExam_day,
+                    mtmExam_time,
+                    mtmExam_duration,
+                    mtmExam_link);
+}
+ExamDetails& ExamDetails::operator=(const ExamDetails& exam)
+{
+    course_number = exam.course_number;
+    month = exam.month;
+    day = exam.day;
+    time = exam.time;
+    duration = exam.duration;
+    link = exam.link;
+    return *this;
+}
+std::string ExamDetails::getLink() const
+{
+    return link;
+}
+void ExamDetails::setLink(const std::string new_link)
+{
+    link = new_link;
+}
+int ExamDetails::operator-(const ExamDetails& exam) const
+{
+    return (month - exam.month)*days_in_month + (day - exam.day);
+}
+bool ExamDetails::operator<(const ExamDetails& exam) const
+{
+    int delta = *this - exam;
+    if(delta > 0){
+        return false;
+    }
+    if(delta < 0){
+        return true;
+    }
+    if(time + duration < exam.time){
+        return true;
+    }
+    return false;
+}
+std::ostream& ExamDetails::operator<<(std::ostream& os, const ExamDetails& exam)
+{
+    double intpart;
+    double fract_part = std::modf(exam.time, &intpart);
+    os << "Course Number:" << exam.course_number << endl;
+    os << "Time:" << exam.day << '.' << exam.month << "at";
+    if(fract_part == 0.5)
+    {
+        os << intpart << ":30" << endl;
+    }
+    else{
+        os << intpart << ":00" << endl;
+    }
+    os << "Duration:" << exam.duration << ":00" << endl;
+    os << "Zoom link:" << exam.link << endl;
+}
--- examDetails.h
+++ examDetails.h
@@ -1,6 +1,7 @@
 #ifndef EXAM_DETAILS_H_
 #define EXAM_DETAILS_H_
 #include <string>
+#include <ostream>
 
 namespace mtm {
     class ExamDetails {
@@ -8,30 +9,30 @@ namespace mtm {
             int course_number;
             int month;
             int day;
-            int time;
+            double time;
             int duration;
-            std::String link;
+            std::string link;
 
         public:
             ExamDetails(int course_number,
                         int month,
                         int day,
-                        int time,
+                        double time,
                         int duration,
-                        std::String link= "");
+                        std::string link= "");
             ExamDetails(const ExamDetails& exam);
-            ~ExamDetails();
-            ExamDetails static makeMatamExam();
+            ~ExamDetails() = default;
+            static ExamDetails makeMatamExam();
             ExamDetails& operator=(const ExamDetails& exam);
-            std::String getLink() const;
-            std::String setLink(const std::String new_link);
+            std::string getLink() const;
+            void setLink(const std::string new_link);
             int operator-(const ExamDetails& exam) const;
             bool operator<(const ExamDetails& exam) const;
-            friend ostream& operator<<(ostream& os, const ExamDetails& exam);
-            class ExamDetails::InvalidDateException {};
-            class ExamDetails::InvalidTimeException {};
-            class ExamDetails::InvalidArgsException {};
-    }
-}
+            friend std::ostream& operator<<(std::ostream& os, const ExamDetails& exam);
+            class InvalidDateException {};
+            class InvalidTimeException {};
+            class InvalidArgsException {};
+    };
+};
 
 #endif /*EXAM_DETAILS_H_*/
